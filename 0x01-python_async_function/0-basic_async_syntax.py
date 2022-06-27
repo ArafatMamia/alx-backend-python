7fbd0aaa0c7164d886b33521f44960974de77e9f
Arafat <arafmamia@gmail.com>
2022-06-27 05:40:38 -0700

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,5 +8,5 @@ import random
 
 async def wait_random(max_delay = 10):
     """ wait random func that have max_delay paramater """
-    rand = random.uniform(0, max_delay)
+    rand = random.random() * max_delay
     return rand 
